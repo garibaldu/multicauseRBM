96a6bd7b884ed0de1c9e21b2a847c6a058a2e6c3
garibaldu <marcus@ecs.vuw.ac.nz>
2015-12-16 16:57:26 +1300

    ea edits

--- Marcus/ea.py
+++ Marcus/ea.py
@@ -27,9 +27,13 @@ if __name__ == '__main__':
     
     if os.path.isfile('./saved_nets/' + opts.nameA + '.npz'):
         A = lbm.RBM(opts.nameA) # attempt to read an existing RBM in.
+    else:
+        sys.exit('no file for A')
+
+    if os.path.isfile('./saved_nets/' + opts.nameB + '.npz'):
         B = lbm.RBM(opts.nameB) # attempt to read an existing RBM in.
     else:
-        sys.exit('you fucked up')
+        sys.exit('no file for B')
 
     A_inpats = inpats
 
@@ -38,24 +42,28 @@ if __name__ == '__main__':
     B_inpats = inpats[rand_order]
 
 
-    #lbm.show_example_images(B_inpats, 'B_inpats.png')
     v = A_inpats + B_inpats
-    #lbm.show_example_images(v, 'combos.png')
 
     A.DROPOUT = False
     B.DROPOUT = False
 
-    # initialise the two hidden layers (cheating a bit)
+    # This would be cheating...
+    #hA = A.pushup(A_inpats)
+    #hB = B.pushup(B_inpats)
+
+    # initialise the two hidden layers (not cheating anymore)
     hA = lbm.random_hiddens_for_rbm(A, num_pats)
     hB = lbm.random_hiddens_for_rbm(B, num_pats)
-    #hB = B.pushup(B_inpats)
+    hA = A.pushup(v)
+    hB = A.pushup(v)
     
-    print (hA)
-    print (hB)
-    plt.imshow(hA)
-    plt.savefig('thing.png')
 
-    lbm.show_example_images(A.pushdown(A.pushup(A_inpats)), 'bollocks.png')
+
+    plt.subplot(121)
+    plt.imshow(hA)
+    plt.subplot(122)
+    plt.imshow(hB)
+    plt.savefig('thing.png')
 
     print ('---------------')
 
